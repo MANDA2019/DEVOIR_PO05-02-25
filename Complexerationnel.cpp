104e08ce9e973bb7870fd162365ed31cdca36ed8
MANDA2019 <bangiromandalina19@gmail.com>
2025-02-06 16:56:26 +0300

    Update Complexerationnel.cpp

---
+++ Complexerationnel.cpp
@@ -0,0 +1,94 @@
+#include "Complexerationnel.h"
+using namespace std;
+
+Complexerationnel::Complexerationnel(int numr,int denumr,int numi,int denumi){
+
+
+
+
+
+}
+Complexerationnel::~Complexerationnel(){
+
+
+
+
+}
+void Complexerationnel::setnumr(int numreel){
+
+
+
+
+
+        }
+void Complexerationnel::setnumi(int numimag){
+
+
+
+        }
+void Complexerationnel::setdenumr(int denumreel){
+
+
+
+
+        }
+void Complexerationnel::setdenumi(int denumimag ){
+
+
+
+
+        }
+int  Complexerationnel::getnumr(){
+
+
+
+        }
+int Complexerationnel::getnumi(){
+
+
+
+        }
+int Complexerationnel::getdenumr(){
+
+
+
+
+
+        }
+int Complexerationnel::getdenumi(){
+
+
+
+
+        }
+Complexerationnel Complexerationnel::addition(Complexerationnel a){
+
+
+
+        }
+Complexerationnel Complexerationnel::soustraction(Complexerationnel a){
+
+
+
+
+
+}
+Complexerationnel Complexerationnel::multiplication(Complexerationnel a){
+
+
+
+
+
+        }
+Complexerationnel Complexerationnel::division(Complexerationnel a){
+
+
+
+        }
+void Complexerationnel::affichage(){
+
+
+
+
+
+        }
